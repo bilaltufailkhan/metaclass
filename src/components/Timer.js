d105bbe0ecaf957744bebcd7b8dffc489cfb0c08
bilaltechnoli <bilal.technoli@gmail.com>
2022-04-17 01:32:17 +0500

    Timer Set

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,42 +1,86 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { calculateTimeLeft } from "../hooks/calculateTimer";
+const STATUS = {
+  STARTED: "Started",
+  STOPPED: "Stopped",
+};
+
+const INITIAL_COUNT = 57600 + 1560 + 60;
 
 const Timer = () => {
-  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft);
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setTimeLeft(calculateTimeLeft());
-    }, 1000);
+  const [secondsRemaining, setSecondsRemaining] = useState(INITIAL_COUNT);
+  const [status, setStatus] = useState(STATUS.STARTED);
 
-    return () => clearTimeout(timer);
-  });
+  const secondsToDisplay = secondsRemaining % 60;
 
-  const timerComponents = [];
+  const minutesRemaining = (secondsRemaining - secondsToDisplay) / 60;
+  const minutesToDisplay = minutesRemaining % 60;
 
-  Object.keys(timeLeft).forEach((interval) => {
-    if (!timeLeft[interval]) {
-      return;
-    }
+  const hoursRemaining = (minutesRemaining - minutesToDisplay) / 60;
+  const hoursToDisplay = hoursRemaining % 60;
 
-    timerComponents.push(
-      <span>
-        {" : "}
-        {timeLeft[interval]}
-      </span>
-    );
-  });
+  const handleStart = () => {
+    setStatus(STATUS.STARTED);
+  };
+  const handleStop = () => {
+    setStatus(STATUS.STOPPED);
+  };
+  const handleReset = () => {
+    setStatus(STATUS.STOPPED);
+    setSecondsRemaining(INITIAL_COUNT);
+  };
+  useInterval(
+    () => {
+      if (secondsRemaining > 0) {
+        setSecondsRemaining(secondsRemaining - 1);
+      } else {
+        setStatus(STATUS.STOPPED);
+      }
+    },
+    status === STATUS.STARTED ? 1000 : null
+    // passing null stops the interval
+  );
+
+  function useInterval(callback, delay) {
+    const savedCallback = useRef();
+
+    // Remember the latest callback.
+    useEffect(() => {
+      savedCallback.current = callback;
+    }, [callback]);
+
+    useEffect(() => {
+      function tick() {
+        savedCallback.current();
+      }
+      if (delay !== null) {
+        let id = setInterval(tick, delay);
+        return () => clearInterval(id);
+      }
+    }, [delay]);
+  }
+
+  // https://stackoverflow.com/a/2998874/1673761
+  const twoDigits = (num) => String(num).padStart(2, "0");
+
   return (
-    <div className="text-center timer py-2 text-white font-weight-bold">
-      Start Sale{" "}
-      {timerComponents.length ? timerComponents : <span>Time's up!</span>}
-      <a
-        href="https://www.pinksale.finance/#/launchpad/0x7AD30e896C88243200023d3A3ACc43A5983931cd?chain=BSC"
-        target="_blank"
-        className="joinNow__btn btn mx-4 font-weight-bold"
-      >
-        Join Now
-      </a>
+    <div className="">
+      <div className="text-center timer py-2 text-white font-weight-bold">
+        Start Sale{" "}
+        {twoDigits(hoursToDisplay) +
+          ":" +
+          twoDigits(minutesToDisplay) +
+          ":" +
+          twoDigits(secondsToDisplay)}
+        <a
+          href="https://www.pinksale.finance/#/launchpad/0x7AD30e896C88243200023d3A3ACc43A5983931cd?chain=BSC"
+          target="_blank"
+          className="joinNow__btn btn mx-4 font-weight-bold"
+        >
+          Join Now
+        </a>
+      </div>
     </div>
   );
 };
--- src/hooks/calculateTimer.js
+++ src/hooks/calculateTimer.js
@@ -1,14 +1,12 @@
-// ...
-
 export const calculateTimeLeft = () => {
   let year = new Date().getFullYear();
-  let difference = +new Date(`10/01/${year}`) - +new Date();
+  let difference = +new Date(`16/04/${year}`) - +new Date();
 
   let timeLeft = {};
 
-  if (difference > 0) {
+  if (difference < 0) {
     timeLeft = {
-      days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+      days: Math.floor(difference / (10000 * 60 * 60 * 24)),
       hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
       minutes: Math.floor((difference / 1000 / 60) % 60),
       seconds: Math.floor((difference / 1000) % 60),
