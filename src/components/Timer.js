369eb09b46407a1ab34c1fc5f7f2e10bf44a59ac
bilaltechnoli <bilal.technoli@gmail.com>
2022-04-16 15:58:08 +0500

    Join now button

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { calculateTimeLeft } from "../hooks/calculateTimer";
 
 const Timer = () => {
@@ -27,6 +28,13 @@ const Timer = () => {
   return (
     <div className="text-center timer py-2">
       {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+      <a
+        href="https://www.pinksale.finance/#/launchpad/0x7AD30e896C88243200023d3A3ACc43A5983931cd?chain=BSC"
+        target="_blank"
+        className="joinNow__btn btn mx-4 font-weight-bold"
+      >
+        Join Now
+      </a>
     </div>
   );
 };
