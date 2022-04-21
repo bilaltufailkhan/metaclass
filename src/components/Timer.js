7a3303322b70311c15b16319bdc8774417bed9ea
Bilal Tufail Khan <66962629+bilaltufailkhan@users.noreply.github.com>
2022-04-21 16:03:19 +0500

    timer set again

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,12 +4,13 @@ import { calculateTimeLeft } from "../hooks/calculateTimer";
 import { useCountdown } from "../utils/timer";
 
 const Timer = () => {
-  // const rebaseTime = useCountdown();
+  const rebaseTime = useCountdown();
 
   return (
     <div className="">
       <div className="text-center timer py-2 text-white font-weight-bold">
         Fair Launch is Live!
+        {`${rebaseTime[1]}:${rebaseTime[2]}:${rebaseTime[3]}`}
         <a
           href="https://www.pinksale.finance/#/launchpad/0x7AD30e896C88243200023d3A3ACc43A5983931cd?chain=BSC"
           target="_blank"
